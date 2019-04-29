004c409c88cd629048e818ba9359de3dbe210481
Đặng Anh Văn <anhvan1999@outlook.com>
2019-04-29 18:07:09 +0700

    Test

--- app/routes/web.php
+++ app/routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Just for test
+Route::get('/test', function() {
+    $catalog = new App\Catalog();
+    $product = new App\Product();
+    $cmt1 = new App\Comment1();
+    $cmt2 = new App\Comment2();
+
+    // Product
+    $product->name = 'Giày addidas';
+    $product->price = 1965000;
+    $product->catalog_id = 1;
+    $product->save();
+    echo "Successful";
+});
