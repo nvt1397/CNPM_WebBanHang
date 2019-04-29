fc06bf4c5164e9b343b5af5ae8a236f6b1981ac7
Đặng Anh Văn <anhvan1999@outlook.com>
2019-04-29 17:38:30 +0700

    Them cac model cho phan cua t

---
+++ app/app/Comment1.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment1 extends Model
+{
+    protected $table = 'comment1';
+}
---
+++ app/app/Comment2.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment2 extends Model
+{
+    protected $table = 'comment2';
+}
