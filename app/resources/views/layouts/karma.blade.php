c41fa56327f509a5c9d8eeedba2eecdd65bcde5e
Đặng Anh Văn <anhvan1999@outlook.com>
2019-05-04 08:55:03 +0700

    Them facebook chat api

--- app/resources/views/layouts/karma.blade.php
+++ app/resources/views/layouts/karma.blade.php
@@ -216,7 +216,31 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
 		<script src={{ asset("js/gmaps.min.js") }}></script>
 		<script src={{ asset("js/main.js") }}></script>
-    @show
+	@show
+	<div id="fb-root"></div>
+	<script>
+	window.fbAsyncInit = function() {
+		FB.init({
+		xfbml            : true,
+		version          : 'v3.3'
+		});
+	};
+
+	(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<!-- Your customer chat code -->
+	<div class="fb-customerchat"
+	attribution=setup_tool
+	page_id="2240697212687648"
+	logged_in_greeting="Xin chào, chúng tôi có thể giúp gì cho bạn?"
+	logged_out_greeting="Xin chào, chúng tôi có thể giúp gì cho bạn?">
+	</div>
 </body>
 
 </html>
